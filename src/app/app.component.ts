acaad7635978e658944458e8145797788e5e0965
rahim jangi <rahimjangi1982@yahoo.com>
2022-05-21 10:33:09 +0200

    finished basic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      username: "nature",
+      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/tree.jpeg"
+    },
+    {
+      title: "Snowy Mountain",
+      username: "mountain lover",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/biking.jpeg"
+    },
+    {
+      title: "Mountain Biking",
+      username: "Biking",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/mountain.jpeg"
+    },
+        {
+      title: "Neat Tree",
+      username: "nature",
+      content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/tree.jpeg"
+    },
+    {
+      title: "Snowy Mountain",
+      username: "mountain lover",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/biking.jpeg"
+    },
+    {
+      title: "Mountain Biking",
+      username: "Biking",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+      imageUrl:"assets/mountain.jpeg"
+    }
+  ];
 }
